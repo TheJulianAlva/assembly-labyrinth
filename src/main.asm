69399d0467d04146b651fcd1ce9303dd6044a6cc
JulianchoAlva <josejulianalvaz@gmail.com>
2026-04-22 00:38:35 -0600

    feat: crea 'main'

---
+++ src/main.asm
@@ -0,0 +1,28 @@
+; ─────────────────────────────────────────────────────────────────────────────
+; src/main.asm
+; Punto de entrada del juego.
+; ─────────────────────────────────────────────────────────────────────────────
+
+%include "constants.inc"
+
+; ── Globales compartidas con otros módulos ────────────────────────────────────
+global player_x
+global player_y
+global current_map_index
+
+section .bss
+
+player_x:           resd 1   ; columna actual del jugador
+player_y:           resd 1   ; fila actual del jugador
+current_map_index:  resd 1   ; índice del mapa activo (0, 1 o 2)
+
+section .text
+
+global _start
+_start:
+    ; inicializar terminal, seleccionar mapa, iniciar game loop
+
+    ; Salida limpia
+    mov eax, SYS_EXIT
+    xor ebx, ebx
+    int 0x80
