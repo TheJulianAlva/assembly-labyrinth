f9a503e3990df911bda5178fba0b69f37627f682
Rockdrigo <rodmjass05@gmail.com>
2026-04-28 13:32:53 -0600

    Mi ruta de ejecución

--- docker-run.bat
+++ docker-run.bat
@@ -1,2 +1,2 @@
 @echo off
-docker run --rm -it -v "C:\Users\julia\OneDrive\Documentos\assembly-labyrinth:/code" -w /code codeneomatrix/nasm sh -c "make run"
+docker run --rm -it -v "C:\Users\Rogelio\OneDrive\Documentos\GitHub\assembly-labyrinth:/code" -w /code codeneomatrix/nasm sh -c "make run"
